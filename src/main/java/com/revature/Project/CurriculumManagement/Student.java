a9bf593b727c0a8795034ff9722303b4f9847492
Ramkumar C <kumarramkumar2000@gmail.com>
2021-07-23 21:14:45 +0530

    Update Student.java

--- src/main/java/com/revature/Project/CurriculumManagement/Student.java
+++ src/main/java/com/revature/Project/CurriculumManagement/Student.java
@@ -1,18 +1,19 @@
 package com.revature.Project.CurriculumManagement;
 
 public class Student {
-	Integer StudentRollNo;
-	String studentName;
-	String studentDOB;
-	String studentStd;
-	String studentAddress;
-	public Student(Integer studentRollNo, String studentName, String studentDOB, String studentStd,
-			String studentAddress) {
+	Integer rollNo;
+	String name;
+	String dateOfBirth;
+	String standard;
+	String address;
+	public Student(Integer rollNo, String name, String dateOfBirth, String standard, String address) {
 		super();
-		this.StudentRollNo = studentRollNo;
-		this.studentName = studentName;
-		this.studentDOB = studentDOB;
-		this.studentStd = studentStd;
-		this.studentAddress = studentAddress;
+		this.rollNo = rollNo;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+		this.standard = standard;
+		this.address = address;
+	}
+	
 	}
 }
