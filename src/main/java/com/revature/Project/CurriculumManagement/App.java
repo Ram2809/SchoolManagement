4e3764ef9b3ea2dc81621b9e21f8f0c97884066f
Ramkumar C <kumarramkumar2000@gmail.com>
2021-07-23 23:31:32 +0530

    Initial Commit

--- src/main/java/com/revature/Project/CurriculumManagement/App.java
+++ src/main/java/com/revature/Project/CurriculumManagement/App.java
@@ -1,7 +1,31 @@
 package com.revature.Project.CurriculumManagement;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+  public static void main(String[] args) throws IOException {
+	  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	  System.out.println("Enter the number of students:");
+	  Integer noOfStudents=Integer.parseInt(br.readLine());
+	  List<Student> studentList=new ArrayList<Student>();
+	  for(Integer i=0;i<noOfStudents;i++)
+	  {
+		  Integer id=Integer.parseInt(br.readLine());
+		  String name=br.readLine();
+		  String dateOfBirth=br.readLine();
+		  String standard=br.readLine();
+		  String address=br.readLine();
+		  studentList.add(new Student(id,name,dateOfBirth,standard,address));
+	  }
+	  Iterator studentIterator=studentList.iterator();
+	  while(studentIterator.hasNext())
+	  {
+		  System.out.println(studentIterator.next());
+	  }
   }
 }
--- src/main/java/com/revature/Project/CurriculumManagement/Student.java
+++ src/main/java/com/revature/Project/CurriculumManagement/Student.java
@@ -1,11 +1,11 @@
 package com.revature.Project.CurriculumManagement;
 
 public class Student {
-	Integer rollNo;
-	String name;
-	String dateOfBirth;
-	String standard;
-	String address;
+	private Integer rollNo;
+	private String name;
+	private String dateOfBirth;
+	private String standard;
+	private String address;
 	public Student(Integer rollNo, String name, String dateOfBirth, String standard, String address) {
 		super();
 		this.rollNo = rollNo;
@@ -15,5 +15,51 @@ public class Student {
 		this.address = address;
 	}
 	
+	public Integer getRollNo() {
+		return rollNo;
 	}
+
+	public void setRollNo(Integer rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getStandard() {
+		return standard;
+	}
+
+	public void setStandard(String standard) {
+		this.standard = standard;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", name=" + name + ", dateOfBirth=" + dateOfBirth + ", standard="
+				+ standard + ", address=" + address + "]";
+	}
+
 }
+
